650d5029440fbf3d3327b9227f431df298370825
Gerben Schevelenbosch <gerben.schevelenbosch@student.uhasselt.be>
2026-04-21 15:32:38 +0200

    Init, werkt voor windows (mij) normaalgezien. Linux nog te bekijken

---
+++ Project CG & VC 2025-2026/main.cpp
@@ -0,0 +1,36 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW init mislukt\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Venster aanmaken mislukt\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW init mislukt\n";
+        return -1;
+    }
+
+    std::cout << "OpenGL versie: " << glGetString(GL_VERSION) << "\n";
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
